eff3c962f5546f500f555679c2ce1cb383d48baa
ronick-grammer <glflakcm@gmail.com>
2021-12-06 17:24:54 +0900

    Feature: UI 프리뷰 코드 작업

--- UnsplashPhoto/ViewController.swift
+++ UnsplashPhoto/ViewController.swift
@@ -16,3 +16,54 @@ class ViewController: UIViewController {
     
 }
 
+
+// MARK: - UI PreView
+#if canImport(SwiftUI) && DEBUG
+
+import SwiftUI
+
+struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+    
+    let viewController: ViewController
+    
+    init(_ builder: @escaping () -> ViewController) {
+        viewController = builder()
+        
+    }
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        return viewController
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+        
+        viewController.view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        viewController.view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        
+    }
+}
+
+
+struct UIViewPreview<View: UIView>: UIViewRepresentable {
+    let view: View
+
+    init(_ builder: @escaping () -> View) {
+        view = builder()
+    }
+
+    // MARK: - UIViewRepresentable
+
+    func makeUIView(context: Context) -> UIView {
+        return view
+    }
+
+    func updateUIView(_ view: UIView, context: Context) {
+        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        
+    }
+}
+#endif
+
+
