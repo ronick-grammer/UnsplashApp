8089890f5c631bc4e226f4884069e503ad274559
ronick-grammer <glflakcm@gmail.com>
2022-05-07 16:51:36 +0900

    test: 유닛테스트 초기 변수 설정 변경

--- UnsplashPhotoTests/ViewModel/Search/SearchViewModelTests.swift
+++ UnsplashPhotoTests/ViewModel/Search/SearchViewModelTests.swift
@@ -16,50 +16,44 @@ import Nimble
 class SearchViewModelTests: XCTestCase {
     
     var sut: SearchViewModel!
-    let disposeBag = DisposeBag()
-    private var searchController = UISearchController()
+    var input: SearchViewModel.Input!
+    var output: SearchViewModel.Output!
+
+    var searchButtonClicked: PublishSubject<Void>!
+    var searchQuery: PublishSubject<String?>!
     
-    private var searchViewModel = SearchViewModel()
-    
-//    let buttonClicked = ControlEvent<Void>(events: Observable<Void>.just(()))
-    let searchButtonClicked = PublishSubject<Void>()
-    let searchQuery = PublishSubject<String?>()
-    
-    lazy var input = SearchViewModel.Input(
-        initialize: BehaviorSubject<Void>.init(value: Void()),
-        searchButtonClicked: searchButtonClicked.asObservable(),
-        searchQuery: searchQuery.asObservable(),
-        page: BehaviorSubject<Int>.init(value: 1),
-        didScroll: Observable.just((300, 700, 730))
-    )
-    
-    lazy var output = searchViewModel.transform(input: input)
+    var disposeBag: DisposeBag!
 
     override func setUp() {
         super.setUp()
-        sut = SearchViewModel()
+        sut = SearchViewModel(searchService: SearchServiceStub())
+        searchButtonClicked = PublishSubject<Void>()
+        searchQuery = PublishSubject<String?>()
         
+        input = SearchViewModel.Input(
+            initialize: BehaviorSubject<Void>.init(value: Void()),
+            searchButtonClicked: searchButtonClicked.asObservable(),
+            searchQuery: searchQuery.asObservable(),
+            page: BehaviorSubject<Int>.init(value: 1),
+            didScroll: Observable.just((300, 700, 730))
+            )
+        output = sut.transform(input: input)
+        
+        disposeBag = DisposeBag()
     }
 
     override func tearDown() {
         sut = nil
+        input = nil
+        output = nil
+        searchButtonClicked = nil
+        searchQuery = nil
+        disposeBag = nil
         super.tearDown()
     }
     
-    func test_test() {
-
-        let s = TestScheduler(initialClock: 0)
-        let ob = s.createHotObservable([
-            .next(1, "a"),
-            .next(2, "b")
-        ])
-
-        let observer = s.createObserver(String.self)
-        ob.subscribe(observer).disposed(by: disposeBag)
-
-        s.start()
-
-        expect(observer.events).to(equal([.next(1, "a"), .next(2, "b")]))
+    func test_init() {
+        XCTAssertEqual(try! output.searchedPhotoes.toBlocking().first()?.count, 10)
     }
     
     
